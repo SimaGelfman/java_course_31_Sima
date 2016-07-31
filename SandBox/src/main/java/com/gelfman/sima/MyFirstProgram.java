317d2173280ffa48275d3ca001ea48faf46d2544
SimaGelfman <GelfmanSima@gmail.com>
2016-07-31 11:08:34 +0300

    changeStyles

--- SandBox/src/main/java/com/gelfman/sima/MyFirstProgram.java
+++ SandBox/src/main/java/com/gelfman/sima/MyFirstProgram.java
@@ -1,11 +1,8 @@
 package com.gelfman.sima;
 
-public class MyFirstProgram{
+public class MyFirstProgram {
 
- public static void main(String[] args) {
-	
-	System.out.println("Hello word");	
 
-}
+
 
 }
