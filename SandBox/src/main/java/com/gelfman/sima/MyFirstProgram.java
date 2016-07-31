eae4de8964f62e50600f83d739bc2ed1e064c15c
SimaGelfman <GelfmanSima@gmail.com>
2016-07-31 14:51:11 +0300

    Add area method and hello

--- SandBox/src/main/java/com/gelfman/sima/MyFirstProgram.java
+++ SandBox/src/main/java/com/gelfman/sima/MyFirstProgram.java
@@ -4,8 +4,31 @@ public class MyFirstProgram {
 
 	public static void main ( String[] args ) {
 
-		System.out.println ( "Hello word" );
+		hello ( "World" );
+		hello( "User" );
+
+		double l = 5;
+		System.out.println ( "The area of a square with side " + l + " = " + area ( l ) );
+
+		double a = 4;
+		double b = 5;
+		System.out.println ( "The area of a rectangle with sides " + a + " and " + "b" + " = " + area ( a,b ) );
+
+
 
 	}
+
+	public static void hello ( String somebody ) {
+		System.out.println ( "Hello, " + somebody + "!" );
+	}
+
+	public static double area(double l){
+		return l * l;
+	}
+
+	public static  double area(double a, double b){
+		return a * b;
+	}
+
 
 }
